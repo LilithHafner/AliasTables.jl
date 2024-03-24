50fd0087a899d434275e15952d017c189db18d3a
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-24 13:52:11 -0500

    add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,13 @@ using Aqua
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(OffsetTables)
     end
-    # Write your tests here.
+
+    @testset "Name" begin
+        @test rand(OffsetTable([1])) == 1
+        @test rand(OffsetTable([0])) == 1
+        @test_throws ArgumentError OffsetTable(Int[])
+        @test rand(OffsetTable([1, 1])) in [1, 2]
+        @test_broken rand(OffsetTable([1, 0])) == 1 # TODO: support 100% probability
+        @test_broken rand(OffsetTable([1, 1, 1])) in [1, 2, 3] # TODO: support non-power of 2 lengths
+    end
 end
