96d5e7db7ff0b09065f14c4bedeb0a8f090ed7f0
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-25 13:18:13 -0500

    use documentervitepress

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,19 @@
 using OffsetTables
 using Documenter
+using DocumenterVitepress
 
 DocMeta.setdocmeta!(OffsetTables, :DocTestSetup, :(using OffsetTables); recursive=true)
 
 makedocs(;
     modules=[OffsetTables],
     authors="Lilith Orion Hafner <lilithhafner@gmail.com> and contributors",
+    repo=Remotes.GitHub("LilithHafner", "OffsetTables.jl"),
     sitename="OffsetTables.jl",
-    format=Documenter.HTML(;
-        canonical="https://LilithHafner.github.io/OffsetTables.jl",
-        edit_link="main",
-        assets=String[],
-    ),
+    format=DocumenterVitepress.MarkdownVitepress(
+        repo = "https://github.com/LilithHafner/OffsetTables.jl",
+        devbranch = "main",
+        devurl = "dev",
+        deploy_url = "chairmarks.lilithhafner.com"),
     pages=[
         "Home" => "index.md",
     ],
