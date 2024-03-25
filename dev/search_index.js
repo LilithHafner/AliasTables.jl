77dfc8bc3d18f727d3f822ccd86a647ab6466afb
Documenter.jl <documenter@juliadocs.github.io>
2024-03-25 01:34:18 +0000

    build based on 4f6c068

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OffsetTables","category":"page"},{"location":"#OffsetTables","page":"Home","title":"OffsetTables","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OffsetTables.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OffsetTables]","category":"page"}]
+}
