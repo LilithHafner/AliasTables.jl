9efc11acb07f2e036c7f405236fe9130c6628035
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-28 11:04:06 -0500

    enable regression tests on v1

--- test/runtests.jl
+++ test/runtests.jl
@@ -134,6 +134,6 @@ using RegressionTests
     end
 
     @testset "RegressionTests" begin
-        v"1.11.0-alpha2" <= VERSION && RegressionTests.test(skip_unsupported_platforms=true)
+        RegressionTests.test(skip_unsupported_platforms=true)
     end
 end
