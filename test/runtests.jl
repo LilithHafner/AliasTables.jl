fe1fda0d5c854573f779006c27b9538dc21edcbf
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-31 09:34:59 -0500

    skip RegressionTests locally

--- test/runtests.jl
+++ test/runtests.jl
@@ -134,6 +134,6 @@ using RegressionTests
     end
 
     @testset "RegressionTests" begin
-        RegressionTests.test(skip_unsupported_platforms=true)
+        "CI" ∈ keys(ENV) && RegressionTests.test(skip_unsupported_platforms=true)
     end
 end
